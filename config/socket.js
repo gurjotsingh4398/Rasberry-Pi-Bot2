e5bb382e74ec23f516ea6bf47e22238f2a855e1a
Gurjot Singh <gurjotsingh4398@gmail.com>
2018-09-29 16:21:33 +0530

    added controls and improved the code

--- config/socket.js
+++ config/socket.js
@@ -1,57 +1,43 @@
-//const PythonShell = require("python-shell");
-
-const pythonOptions = {
-  mode: "text"
-  // pythonPath: '/usr/bin/python',
-  // scriptPath: '/home/pi/Robot4x4Raspberry/'
-};
+const controls = require("./controls");
 
 let path = [];
 
 module.exports = socket => {
-  // var robot = initController();
-
   socket.on("connection", user => {
     console.log("connected server");
     user.on("arrow", state => {
-      //robot.send(state + "\n"); //sending to python file the state of robot
       if (state.state == "stop") {
         socket.emit("actualState", state.state); // broadcasting to every user
+        controls.stop();
+        console.log("stopped");
+        path.push({
+          direction: state.direction,
+          time: state.time
+        });
+        console.log(path);
       } else {
         let str = "";
         if (state == "up") {
           str = "Moving Forward";
+          console.log("forwad called");
+          controls.forward();
+          console.log("forward done");
         }
         if (state == "left") {
           str = "Moving Left";
+          controls.left();
         }
         if (state == "right") {
           str = "Moving Right";
+          controls.right();
         }
         if (state == "down") {
           str = "Moving Backward";
+          controls.backward();
         }
         socket.emit("actualState", str); // broadcasting to every user
+        console.log("all done");
       }
     });
   });
-
-  // process.on("SIGINT", function() {
-  //   robot.send("close");
-  //   robot.end(0);
-  // });
 };
-
-// function initController() {
-//   var pyshell = new PythonShell("robot.py", pythonOptions);
-
-//   pyshell.on("message", function(message) {
-//     console.log("[ROBOT] Message: " + message);
-//   });
-
-//   pyshell.on("error", function(err) {
-//     console.log("[ROBOT] Not possible to communicate with the Robot.\n" + err);
-//   });
-
-//   return pyshell;
-// }
